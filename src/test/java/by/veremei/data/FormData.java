b424e47d7e0d4e07a3f6abfa642eb1796dd3bf1f
Oleg Veremei <veremeioleg@gmail.com>
2023-12-19 17:42:34 +0100

    Delete RandomUtils, refactor BaseTest & added faker.regexify to testData

--- src/test/java/by/veremei/data/FormData.java
+++ src/test/java/by/veremei/data/FormData.java
@@ -4,12 +4,10 @@ import com.github.javafaker.Faker;
 
 import java.util.Locale;
 
-import static by.veremei.utils.RandomUtils.getRandomUtils;
-
 public class FormData {
     private final Faker euFaker = new Faker(new Locale("en"));
     public final String validEmail = "test@test.com",
                         randomEmail = euFaker.internet().emailAddress(),
                         invalidEmail = randomEmail.replace("@", "!").replace(".", "_"),
-                        shortDescr = getRandomUtils(100);
+                        shortDescr = euFaker.regexify("[a-z1-9]{100}");
 }
--- src/test/java/by/veremei/test/BaseTest.java
+++ src/test/java/by/veremei/test/BaseTest.java
@@ -6,14 +6,15 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
 public class BaseTest {
-    @BeforeEach
-    void init() {
+    @BeforeAll
+    static void beforeAll() {
         Configuration.baseUrl = "https://devquality.ru/";
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
@@ -27,7 +28,10 @@ public class BaseTest {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
 
+    @BeforeEach
+    void init() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
